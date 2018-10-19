20eeed670bf89880b1509cbab159b90d0353d147
Jhonny Moreira <me.jhonnymoreira@gmail.com>
2018-10-19 00:07:43 -0300

    Format supported files

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,21 +6,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
-)
+const Hello = ({ name }) => <div>Hello {name}!</div>
 
 Hello.defaultProps = {
-  name: 'David'
+  name: 'David',
 }
 
 Hello.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
+    document.body.appendChild(document.createElement('div'))
   )
 })
