bf9fecc9acf116cf8e6cdefb090c9669a61238a6
Jhonny Moreira <me.jhonnymoreira@gmail.com>
2018-10-22 13:40:11 -0300

    Counter tests with "jest-enzyme" matchers

--- app/javascript/components/Counter/index.test.js
+++ app/javascript/components/Counter/index.test.js
@@ -11,23 +11,23 @@ describe("Dumb Counter component", () => {
     it("accepts an initial counter value as props", () => {
       const wrapper = mount(<Counter initialValue={1} />)
   
-      expect(wrapper.state("counter")).toEqual(1);
+      expect(wrapper).toHaveState('counter', 1)
     });
 
     it("defaults the initial counter value to 0", () => {
-      expect(wrapper.state('counter')).toEqual(0)
+      expect(wrapper).toHaveState('counter', 0)
     })
   })
 
   it("increments the counter value on button click", () => {
     wrapper.find("#increment").simulate("click");
 
-    expect(wrapper.state("counter")).toEqual(1);
+    expect(wrapper).toHaveState('counter', 1)
   });
 
   it("decrements the counter value on button click", () => {
     wrapper.find('#decrement').simulate('click')
 
-    expect(wrapper.state("counter")).toEqual(-1)
+    expect(wrapper).toHaveState('counter', -1)
   })
 });
