d70ddc0cd40ee697506a6a8b22bbb61437b176fa
Jhonny Moreira <me.jhonnymoreira@gmail.com>
2018-10-23 00:11:53 -0300

    Format code

--- app/javascript/components/Counter/index.js
+++ app/javascript/components/Counter/index.js
@@ -1,18 +1,18 @@
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
 
 class Counter extends Component {
   state = {
-    counter: this.props.initialValue || 0
-  };
+    counter: this.props.initialValue || 0,
+  }
 
   decrement = () => {
-    this.setState(({ counter }) => ({ counter: counter - 1 }));
-  };
+    this.setState(({ counter }) => ({ counter: counter - 1 }))
+  }
 
   increment = () => {
-    this.setState(({ counter }) => ({ counter: counter + 1 }));
-  };
+    this.setState(({ counter }) => ({ counter: counter + 1 }))
+  }
 
   render() {
     return (
@@ -21,12 +21,12 @@ class Counter extends Component {
         <p>Current value: {this.state.counter}</p>
         <button id="increment" type="button" onClick={this.increment} />
       </Fragment>
-    );
+    )
   }
 }
 
 Counter.propTypes = {
-  initialValue: PropTypes.number
-};
+  initialValue: PropTypes.number,
+}
 
-export default Counter;
+export default Counter
--- app/javascript/components/Counter/index.test.js
+++ app/javascript/components/Counter/index.test.js
@@ -1,6 +1,6 @@
-import Counter from ".";
+import Counter from '.'
 
-describe("Dumb Counter component", () => {
+describe('Dumb Counter component', () => {
   let wrapper
 
   beforeEach(() => {
@@ -8,26 +8,26 @@ describe("Dumb Counter component", () => {
   })
 
   describe('state', () => {
-    it("accepts an initial counter value as props", () => {
+    it('accepts an initial counter value as props', () => {
       const wrapper = mount(<Counter initialValue={1} />)
-  
-      expect(wrapper).toHaveState('counter', 1)
-    });
 
-    it("defaults the initial counter value to 0", () => {
+      expect(wrapper).toHaveState('counter', 1)
+    })
+
+    it('defaults the initial counter value to 0', () => {
       expect(wrapper).toHaveState('counter', 0)
     })
   })
 
-  it("increments the counter value on button click", () => {
-    wrapper.find("#increment").simulate("click");
+  it('increments the counter value on button click', () => {
+    wrapper.find('#increment').simulate('click')
 
     expect(wrapper).toHaveState('counter', 1)
-  });
+  })
 
-  it("decrements the counter value on button click", () => {
+  it('decrements the counter value on button click', () => {
     wrapper.find('#decrement').simulate('click')
 
     expect(wrapper).toHaveState('counter', -1)
   })
-});
+})
